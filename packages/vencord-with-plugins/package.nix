21b56db13a3a2b1b3eaba3ff6b3ca3c153aa27c0
IRONM00N <me@ironmoon.dev>
2025-10-21 08:11:13 -0400

    bump

--- packages/vencord-with-plugins/package.nix
+++ packages/vencord-with-plugins/package.nix
@@ -4,47 +4,43 @@
 }:
 let
   inherit (pkgs.lib) toList escapeShellArg;
-  resolvedPlugins = map (
-    plugin:
-    let
-      path = if plugin ? outPath then plugin.outPath else plugin;
-      name = baseNameOf (toString path);
-    in
-    {
-      inherit path name;
-    }
-  ) (toList userPlugins);
+  resolvedPlugins =
+    (toList userPlugins)
+    |> map (
+      plugin:
+      let
+        path = if plugin ? outPath then plugin.outPath else plugin;
+        name = baseNameOf (toString path);
+      in
+      {
+        inherit path name;
+      }
+    );
 in
 pkgs.vencord.overrideAttrs (old: {
-
-  src = pkgs.fetchFromGitHub {
-    owner = "Vendicated";
-    repo = "Vencord";
-    rev = "a33e81d1cbd7ab50c0b1e1446c925bf259e671fc";
-    hash = "sha256-7JT8BMKUhIwYMkIwr2mD8IQLDpldcDtAKh6R1tbAKMw=";
-    # hash = pkgs.lib.fakeHash;
-  };
 
   preBuild =
     (old.preBuild or "")
     + ''
       mkdir -p src/userplugins
-      ${pkgs.lib.concatStringsSep "\n" (
-        map (
+      ${
+        resolvedPlugins
+        |> map (
           { path, name }:
           ''
             mkdir -p src/userplugins/${name}
             cp -r ${escapeShellArg path}/* src/userplugins/${name}/
           ''
-        ) resolvedPlugins
-      )}
+        )
+        |> pkgs.lib.concatStringsSep "\n"
+      }
     '';
 
   patches = (old.patches or [ ]) ++ [
     ./remove-support-warning.patch
   ];
 
-  patchFlags = (old.patches or [ ]) ++ [
+  patchFlags = (old.patchFlags or [ ]) ++ [
     "-p1"
     "--no-backup-if-mismatch"
   ];
