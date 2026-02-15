3c4173e8311841360ac4f06c82ddbf80a87d4714
IRONM00N <me@ironmoon.dev>
2026-02-15 00:24:18 -0500

    bump; bundles(nvidia): CachyOS kernel patch for kernel 6.19

--- bundles/nvidia.nix
+++ bundles/nvidia.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 let
@@ -28,8 +29,25 @@ in
       # these are not yet ready:
       open = true;
       nvidiaSettings = true; # nvidia-settings
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+      # TODO: remove once https://github.com/NixOS/nixpkgs/issues/489947 is fixed
+      package =
+        let
+          base = config.boot.kernelPackages.nvidiaPackages.latest;
+          cachyos-nvidia-patch = pkgs.fetchpatch {
+            url = "https://raw.githubusercontent.com/CachyOS/CachyOS-PKGBUILDS/master/nvidia/nvidia-utils/kernel-6.19.patch";
+            sha256 = "sha256-YuJjSUXE6jYSuZySYGnWSNG5sfVei7vvxDcHx3K+IN4=";
+          };
+          driverAttr = if config.hardware.nvidia.open then "open" else "bin";
+        in
+        base
+        // {
+          ${driverAttr} = base.${driverAttr}.overrideAttrs (oldAttrs: {
+            patches = (oldAttrs.patches or [ ]) ++ [ cachyos-nvidia-patch ];
+          });
+        };
     };
+
     # Load nvidia driver for Xorg and Wayland
     services.xserver.videoDrivers = [ "nvidia" ];
   };
